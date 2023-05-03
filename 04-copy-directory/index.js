9c456cc1375b247d6a9bc133d266fbbf339145f9
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-03 18:13:41 +0300

    feat: add copy source folder to target folder. processing is recursive

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require("fs");
+const path = require("path");
 
+const dirPath = path.resolve(__dirname, "files");
+const targetPath = path.resolve(__dirname, "files-copy");
+
+if (fs.existsSync(targetPath)) {
+  fs.rmdir(targetPath, () => {});
+}
+fs.mkdir(targetPath, () => {});
+
+function reading(directory, target) {
+  fs.readdir(directory, { withFileTypes: true }, (err, files) => {
+    if (err) console.log(err);
+    else {
+      for (let i = 0; i < files.length; i++) {
+        const sourceFile = path.resolve(directory, files[i].name);
+        const targetFile = path.resolve(target, files[i].name);
+        fs.stat(sourceFile, (err, stats) => {
+          if (err) console.log(err);
+          if (stats.isFile()) {
+            const readStream = fs.createReadStream(sourceFile);
+            const writeStream = fs.createWriteStream(targetFile);
+            readStream.pipe(writeStream);
+          } else {
+            fs.mkdir(targetFile, () => {});
+            reading(sourceFile, targetFile);
+          }
+        });
+      }
+    }
+  });
+}
+
+reading(dirPath, targetPath);
